45bf98db0d9508fbf077a89a6bc631e1a0db61d7
root <root@localhost.localdomain>
2017-04-01 12:38:16 -0400

    fibonacci

--- examples.cpp
+++ examples.cpp
@@ -6,4 +6,30 @@
 int main()
 {
 	Grid<int> g;
+
+	FibonacciIterator<int> f;
+
+	std::cout << *f << std::endl;
+
+	f++;
+
+	std::cout << *f << std::endl;
+
+	f++;
+
+
+	std::cout << *f << std::endl;
+
+
+	f++;
+	f++;
+	f++;
+	std::cout << *f << std::endl;
+
+	f--;
+
+	std::cout << *f << std::endl;
+
+
+
 }
--- fibonacci.h
+++ fibonacci.h
@@ -38,9 +38,75 @@ template <typename Int, typename Plus, typename Minus>
 bool operator== (const FibonacciIterator<Int, Plus, Minus>& lhs, 
 		 const FibonacciIterator<Int, Plus, Minus>& rhs);
 
+template <typename Int, typename Plus, typename Minus>
+bool operator!= (const FibonacciIterator<Int, Plus, Minus>& lhs,
+		 const FibonacciIterator<Int, Plus, Minus>& rhs);
+
+template <typename Int, typename Plus, typename Minus>
+FibonacciIterator<Int,Plus, Minus>::FibonacciIterator(Int zero,
+						      Int one,
+						      Plus plus,
+						      Minus minus)
+	: curr(zero), next(one), plus(plus), minus(minus) {
+
+}
 
 
+template <typename Int, typename Plus, typename Minus>
+const Int& FibonacciIterator<Int, Plus, Minus>::operator* () const {
+	return curr;
+}
 
+template <typename Int, typename Plus, typename Minus>
+const Int* FibonacciIterator<Int, Plus, Minus>::operator-> () const {
+	return &**this;
+}
+
+template <typename Int, typename Plus, typename Minus>
+FibonacciIterator<Int, Plus, Minus>&
+FibonacciIterator<Int, Plus, Minus>::operator++ () {
+	Int newNext = plus(curr, next);
+	curr = next;
+	next = newNext;
+	return *this;
+}
+
+template <typename Int, typename Plus, typename Minus>
+const FibonacciIterator<Int, Plus, Minus>
+FibonacciIterator<Int, Plus, Minus>::operator++ (int) {
+	FibonacciIterator result = *this;
+	++ *this;
+	return result;
+}
+
+template <typename Int, typename Plus, typename Minus>
+FibonacciIterator<Int, Plus, Minus>&
+FibonacciIterator<Int, Plus, Minus>::operator-- () {
+	Int prev = minus(next, curr);
+	next = curr;
+	curr = prev;
+	return *this;
+}
+
+template <typename Int, typename Plus, typename Minus>
+const FibonacciIterator<Int, Plus, Minus>
+FibonacciIterator<Int, Plus, Minus>::operator-- (int) {
+	FibonacciIterator result = *this;
+	-- *this;
+	return result;
+}
+
+template <typename Int, typename Plus, typename Minus>
+bool operator== (const FibonacciIterator<Int, Plus, Minus>& lhs,
+		const FibonacciIterator<Int, Plus, Minus>& rhs) {
+	return lhs.curr == rhs.curr && lhs.next == rhs.next;
+}
+
+template <typename Int, typename Plus, typename Minus>
+bool operator!= (const FibonacciIterator<Int, Plus, Minus>& lhs,
+		const FibonacciIterator<Int, Plus, Minus>& rhs) {
+	return !(lhs == rhs);
+}
 
 
 
