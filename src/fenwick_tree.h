63408d81efecf46915af316c5d34a6baf9614922
Vivek Soundararaj <vivegau@gmail.com>
2018-09-29 22:45:09 -0400

    quick fix on fenwick...

--- src/fenwick_tree.h
+++ src/fenwick_tree.h
@@ -2,36 +2,36 @@
 #include "./utils/print_utils.h"
 #include <type_traits>
 
-template<typename T>
+template<typename T, typename IT=int>
 class Fenwick {
     // static_assert(std::is_base_of<int, T>::value, "T must be of type");
 
     private:
-        T n;
+        IT n;
         std::vector<T> v;
 
     public:
-        explicit Fenwick(T n);
+        explicit Fenwick(IT n);
         Fenwick(const Fenwick&) = delete;
         Fenwick& operator=(Fenwick const&) = delete;
 
-        void update(T i, T val);
-        T rangeSum(T i);
+        void update(IT i, T val);
+        T rangeSum(IT i);
 };
 
-template<typename T>
-Fenwick<T>::Fenwick(T n):n(n), v(n, 0){
+template<typename T, typename IT>
+Fenwick<T, IT>::Fenwick(IT n):n(n), v(n, 0){
 }
 
-template<typename T>
-void Fenwick<T>::update(T i, T val){
+template<typename T, typename IT>
+void Fenwick<T, IT>::update(IT i, T val){
     for (; i <= n; i += i&-i){
         v[i-1] += val;
     }
 }
 
-template<typename T>
-T Fenwick<T>::rangeSum(T i){
+template<typename T, typename IT>
+T Fenwick<T, IT>::rangeSum(IT i){
     T sum = 0;
     for(; i > 0; i -= i&-i)
         sum += v[i-1];
